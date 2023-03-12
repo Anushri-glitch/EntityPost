2f06e505894fd71163e137bbb411333b921010f2
Anuhshka Srivastava <shrishtisrivastava19@gmail.com>
2023-03-12 21:05:37 +0530

    Post new Commit

---
+++ src/main/java/com/Anushka/entityPost/EntityPostApplication.java
@@ -0,0 +1,13 @@
+package com.Anushka.entityPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityPostApplication.class, args);
+	}
+
+}
